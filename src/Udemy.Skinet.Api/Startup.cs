66945c3f3b182fd2bb4723dce1c88591e6489f1c
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-16 22:30:02 +0200

    02-012 add DbContext to services with Sqlite

--- src/Udemy.Skinet.Api/Startup.cs
+++ src/Udemy.Skinet.Api/Startup.cs
@@ -1,20 +1,23 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Udemy.Skinet.Api.Data;
 
 namespace Udemy.Skinet.Api {
     public class Startup {
-        public Startup(IConfiguration configuration) {
-            Configuration = configuration;
-        }
+        private readonly IConfiguration _config;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) {
+            _config = configuration;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
