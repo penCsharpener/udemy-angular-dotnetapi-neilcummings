a804c89389933f506465b2c5b77b9d2c0256ddc4
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-16 22:23:37 +0200

    02-011 add EF Core and Sqlite with StoreContext

---
+++ src/Udemy.Skinet.Api/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Udemy.Skinet.Api.Entities;
+
+namespace Udemy.Skinet.Api.Data {
+    public class StoreContext : DbContext {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
