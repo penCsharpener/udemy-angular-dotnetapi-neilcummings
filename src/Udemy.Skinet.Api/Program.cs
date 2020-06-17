6db2ab4cdb0e85c0d3ae7bb3bba497f1c6592184
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-17 23:45:11 +0200

    03-027 applying migrations, creating database at app startup

--- src/Udemy.Skinet.Api/Program.cs
+++ src/Udemy.Skinet.Api/Program.cs
@@ -1,10 +1,30 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Udemy.Skinet.Infrastructure.Data;
 
 namespace Udemy.Skinet.Api {
     public sealed class Program {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+        public static async Task Main(string[] args) {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                } catch (System.Exception ex) {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration.");
+                }
+
+                host.Run();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
