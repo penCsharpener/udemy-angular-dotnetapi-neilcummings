373082a25a970e551d42c6aa6c9548e44c4e6739
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-18 23:55:12 +0200

    04-038 implementing the repo with specification methods

--- src/Udemy.Skinet.Infrastructure/Data/GenericRepository.cs
+++ src/Udemy.Skinet.Infrastructure/Data/GenericRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Udemy.Skinet.Core.Entities;
 using Udemy.Skinet.Core.Interfaces;
+using Udemy.Skinet.Core.Specifications;
 
 namespace Udemy.Skinet.Infrastructure.Data {
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity {
@@ -14,13 +15,23 @@ namespace Udemy.Skinet.Infrastructure.Data {
         }
 
         public async Task<T> GetByIdAsync(int id) {
-            throw new NotImplementedException();
             return await _context.Set<T>().FindAsync(id);
         }
 
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec) {
+            return await ApplySpecification(spec).SingleOrDefaultAsync();
+        }
+
         public async Task<IReadOnlyList<T>> ListAllAsync() {
-            throw new NotImplementedException();
             return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec) {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec) {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
