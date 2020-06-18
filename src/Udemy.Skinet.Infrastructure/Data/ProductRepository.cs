7d06b554992f128621c07979386b72c554edd799
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-18 23:17:33 +0200

    03-030 eager loading of navigation properties

--- src/Udemy.Skinet.Infrastructure/Data/ProductRepository.cs
+++ src/Udemy.Skinet.Infrastructure/Data/ProductRepository.cs
@@ -17,11 +17,13 @@ namespace Udemy.Skinet.Infrastructure.Data {
         }
 
         public async Task<Product> GetProductByIdAsync(int id) {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products.Include(p => p.ProductBrand).Include(p => p.ProductType).SingleOrDefaultAsync(p => p.Id.Equals(id));
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync() {
-            return await _context.Products.ToListAsync();
+            return await _context.Products.Include(p => p.ProductBrand)
+                                          .Include(p => p.ProductType)
+                                          .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync() {
