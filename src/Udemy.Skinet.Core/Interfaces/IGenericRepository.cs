c57d1c58920c43faea94c768ceaf2640a20ecaa9
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-18 23:35:27 +0200

    04-033 creating a generic repository and interface

---
+++ src/Udemy.Skinet.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Udemy.Skinet.Core.Entities;
+
+namespace Udemy.Skinet.Core.Interfaces {
+    public interface IGenericRepository<T> where T : BaseEntity {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+    }
+}
---
+++ src/Udemy.Skinet.Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Udemy.Skinet.Core.Entities;
+using Udemy.Skinet.Core.Interfaces;
+
+namespace Udemy.Skinet.Infrastructure.Data {
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity {
+        public async Task<T> GetByIdAsync(int id) {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync() {
+            throw new NotImplementedException();
+        }
+    }
+}
