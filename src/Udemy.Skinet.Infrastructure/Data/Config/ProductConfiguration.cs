468fb56d79d31125db829119f67ff78996915c0c
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-18 23:02:55 +0200

    02-028 fix typo in PictureUrl property

--- src/Udemy.Skinet.Infrastructure/Data/Config/ProductConfiguration.cs
+++ src/Udemy.Skinet.Infrastructure/Data/Config/ProductConfiguration.cs
@@ -9,7 +9,7 @@ namespace Udemy.Skinet.Infrastructure.Data.Config {
             builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
             builder.Property(p => p.Description).IsRequired().HasMaxLength(180);
             builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18,2)");
-            builder.Property(p => p.PrictureUrl).IsRequired();
+            builder.Property(p => p.PictureUrl).IsRequired();
             builder.HasOne(b => b.ProductBrand).WithMany().HasForeignKey(p => p.ProductBrandId);
             builder.HasOne(b => b.ProductType).WithMany().HasForeignKey(p => p.ProductTypeId);
         }
