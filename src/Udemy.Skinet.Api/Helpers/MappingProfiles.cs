056f00504282c3da902ab44cb9b63bf4333ba8cd
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-19 21:38:59 +0200

    04-045 adding a custom value resolver for AutoMapper

--- src/Udemy.Skinet.Api/Helpers/MappingProfiles.cs
+++ src/Udemy.Skinet.Api/Helpers/MappingProfiles.cs
@@ -7,7 +7,8 @@ namespace Udemy.Skinet.Api.Helpers {
         public MappingProfiles() {
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
         }
     }
 }
---
+++ src/Udemy.Skinet.Api/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Udemy.Skinet.Api.Dtos;
+using Udemy.Skinet.Core.Entities;
+
+namespace Udemy.Skinet.Api.Helpers {
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string> {
+        private readonly IConfiguration _configuration;
+
+        public ProductUrlResolver(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context) {
+            if (!string.IsNullOrEmpty(source.PictureUrl)) {
+                return _configuration["ApiUrl"].Trim('/') + "/" + source.PictureUrl.Trim('/');
+            }
+
+            return null;
+        }
+    }
+}
