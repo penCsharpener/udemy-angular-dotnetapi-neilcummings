a8b293fa643ebbc2d23373caf2a9e3eceb1e7e8f
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-19 23:54:03 +0200

    05-056 cleaning up the startup class

---
+++ src/Udemy.Skinet.Api/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Udemy.Skinet.Api.Errors;
+using Udemy.Skinet.Core.Interfaces;
+using Udemy.Skinet.Infrastructure.Data;
+
+namespace Udemy.Skinet.Api.Extensions {
+    public static class ApplicationServicesExtensions {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services) {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            // needs to be configured last after the above
+            services.Configure<ApiBehaviorOptions>(opt => {
+                opt.InvalidModelStateResponseFactory = actionContext => {
+                    var errors = actionContext.ModelState.Where(err => err.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage)
+                    .ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+
+            return services;
+        }
+    }
+}
--- src/Udemy.Skinet.Api/Startup.cs
+++ src/Udemy.Skinet.Api/Startup.cs
@@ -1,16 +1,12 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using System.Linq;
-using Udemy.Skinet.Api.Errors;
+using Udemy.Skinet.Api.Extensions;
 using Udemy.Skinet.Api.Helpers;
 using Udemy.Skinet.Api.Middleware;
-using Udemy.Skinet.Core.Interfaces;
 using Udemy.Skinet.Infrastructure.Data;
 
 namespace Udemy.Skinet.Api {
@@ -23,33 +19,12 @@ namespace Udemy.Skinet.Api {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-            // needs to be configured last after the above
-            services.Configure<ApiBehaviorOptions>(opt => {
-                opt.InvalidModelStateResponseFactory = actionContext => {
-                    var errors = actionContext.ModelState.Where(err => err.Value.Errors.Count > 0)
-                    .SelectMany(x => x.Value.Errors)
-                    .Select(x => x.ErrorMessage)
-                    .ToArray();
-
-                    var errorResponse = new ApiValidationErrorResponse {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo {
-                    Title = "SkiNet API",
-                    Version = "v1"
-                });
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,10 +39,7 @@ namespace Udemy.Skinet.Api {
             app.UseStaticFiles();
 
             app.UseAuthorization();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet API v1");
-            });
+            app.UseSwaggerDocumenation();
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
