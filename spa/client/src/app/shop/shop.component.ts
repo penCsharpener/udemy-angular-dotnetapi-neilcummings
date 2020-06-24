80b2f0db7f388b4672e8def5d609732d038c6a76
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-24 11:59:04 +0200

    09-104 resolving the bug with pagination page >1

--- spa/client/src/app/shop/shop.component.ts
+++ spa/client/src/app/shop/shop.component.ts
@@ -69,11 +69,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -89,6 +91,7 @@ export class ShopComponent implements OnInit {
 
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
