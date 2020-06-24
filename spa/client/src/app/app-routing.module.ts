115746c5f7629c698f9641bf8e963aca30860145
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-24 23:33:26 +0200

    11-116 creating an error component for testing the error responses, using environment.ts

--- spa/client/src/app/app-routing.module.ts
+++ spa/client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'test-error', component: TestErrorComponent },
   {
     path: 'shop',
     loadChildren: () =>
