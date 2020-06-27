553168ced3bceb113bb6fc7e88e10ddfe624aeee
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-27 23:24:40 +0200

    13-136 setting up the basket class

---
+++ src/Udemy.Skinet.Core/Entities/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Udemy.Skinet.Core.Entities {
+    public class CustomerBasket {
+        public CustomerBasket() { }
+
+        public CustomerBasket(string id) {
+            Id = id;
+        }
+
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
