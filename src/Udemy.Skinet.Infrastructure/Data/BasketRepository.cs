c89e0b55f3af09a4882530070093487c81f96b25
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-27 23:33:38 +0200

    13-138 implementing the basket repository

--- src/Udemy.Skinet.Infrastructure/Data/BasketRepository.cs
+++ src/Udemy.Skinet.Infrastructure/Data/BasketRepository.cs
@@ -1,20 +1,36 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Udemy.Skinet.Core.Entities;
 using Udemy.Skinet.Core.Interfaces;
 
 namespace Udemy.Skinet.Infrastructure.Data {
     public class BasketRepository : IBasketRepository {
-        public Task<bool> DeleteBasketAsync(string basketId) {
-            throw new NotImplementedException();
+        private readonly IDatabase _db;
+
+        public BasketRepository(IConnectionMultiplexer redis) {
+            _db = redis.GetDatabase();
         }
 
-        public Task<CustomerBasket> GetBasketAsync(string basketId) {
-            throw new NotImplementedException();
+        public async Task<bool> DeleteBasketAsync(string basketId) {
+            return await _db.KeyDeleteAsync(basketId);
         }
 
-        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket) {
-            throw new NotImplementedException();
+        public async Task<CustomerBasket> GetBasketAsync(string basketId) {
+            var data = await _db.StringGetAsync(basketId);
+
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
+        }
+
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket) {
+            var created = await _db.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+
+            if (!created) {
+                return null;
+            }
+
+            return await GetBasketAsync(basket.Id);
         }
     }
 }
