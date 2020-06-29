745d323475b56d477d48adbe37d7ac65877b7ec0
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-29 23:41:38 +0200

    15-163 adding the IdentityDbContext

--- src/Udemy.Skinet.Api/Startup.cs
+++ src/Udemy.Skinet.Api/Startup.cs
@@ -9,6 +9,7 @@ using Udemy.Skinet.Api.Helpers;
 using Udemy.Skinet.Api.Middleware;
 using Udemy.Skinet.Infrastructure.Data;
 using Udemy.Skinet.Infrastructure.Extensions;
+using Udemy.Skinet.Infrastructure.Identity;
 
 namespace Udemy.Skinet.Api {
     public class Startup {
@@ -24,6 +25,8 @@ namespace Udemy.Skinet.Api {
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<AppIdentityDbContext>(x => x.UseSqlite(_config.GetConnectionString("IdentityConnection")));
 
             services.AddRedis(_config);
 
---
+++ src/Udemy.Skinet.Infrastructure/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Udemy.Skinet.Infrastructure.Identity {
+    public class AppIdentityDbContext : IdentityDbContext {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+        }
+    }
+}
