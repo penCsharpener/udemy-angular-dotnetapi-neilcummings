bbb54163141f56b6ace9b350c45b02598cf94b2c
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-30 22:57:31 +0200

    15-168 adding an account controller

---
+++ src/Udemy.Skinet.Api/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Udemy.Skinet.Api.Dtos;
+using Udemy.Skinet.Api.Errors;
+using Udemy.Skinet.Core.Entities.Identity;
+
+namespace Udemy.Skinet.Api.Controllers {
+    public class AccountController : BaseApiController {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto) {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) {
+                return Unauthorized(new ApiResponse(System.Net.HttpStatusCode.Unauthorized));
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) {
+                return Unauthorized(new ApiResponse(System.Net.HttpStatusCode.Unauthorized));
+            }
+
+            return new UserDto {
+                Email = user.Email,
+                Token = "this is the token",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
