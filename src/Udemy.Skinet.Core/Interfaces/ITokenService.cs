ecf6c3ae7e57fe0e11428fc73318bfd0491a68ae
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-30 23:14:42 +0200

    15-170 adding a token generation service

---
+++ src/Udemy.Skinet.Core/Interfaces/ITokenService.cs
@@ -0,0 +1,7 @@
+﻿using Udemy.Skinet.Core.Entities.Identity;
+
+namespace Udemy.Skinet.Core.Interfaces {
+    public interface ITokenService {
+        string CreateToken(AppUser user);
+    }
+}
---
+++ src/Udemy.Skinet.Infrastructure/Services/TokenService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Udemy.Skinet.Core.Entities.Identity;
+using Udemy.Skinet.Core.Interfaces;
+
+namespace Udemy.Skinet.Infrastructure.Services {
+    public class TokenService : ITokenService {
+        private readonly IConfiguration _config;
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config) {
+            _config = config;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
+        }
+
+        public string CreateToken(AppUser user) {
+            var claims = new List<Claim> {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.GivenName, user.DisplayName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
+                Issuer = _config["Token:Issuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
