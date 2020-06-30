e553a0b5f232a5696de68fa7a7f649a55bf53ab0
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-06-30 22:31:55 +0200

    15-167 adding identity to program class

--- src/Udemy.Skinet.Api/Program.cs
+++ src/Udemy.Skinet.Api/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using Udemy.Skinet.Core.Entities.Identity;
 using Udemy.Skinet.Infrastructure.Data;
+using Udemy.Skinet.Infrastructure.Identity;
 
 namespace Udemy.Skinet.Api {
     public sealed class Program {
@@ -19,6 +22,11 @@ namespace Udemy.Skinet.Api {
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
                     await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+                    await identityContext.Database.MigrateAsync();
+                    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
                 } catch (System.Exception ex) {
                     var logger = loggerFactory.CreateLogger<Program>();
                     logger.LogError(ex, "An error occured during migration.");
