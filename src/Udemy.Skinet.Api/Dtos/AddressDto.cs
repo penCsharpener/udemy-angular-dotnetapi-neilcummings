bb2baad3746c235b68f33173718ef61b54610565
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-07-01 22:25:32 +0200

    16-180 address model validation with annotations

--- src/Udemy.Skinet.Api/Dtos/AddressDto.cs
+++ src/Udemy.Skinet.Api/Dtos/AddressDto.cs
@@ -1,10 +1,18 @@
-﻿namespace Udemy.Skinet.Api.Dtos {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Udemy.Skinet.Api.Dtos {
     public class AddressDto {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Zipcode { get; set; }
     }
 }
