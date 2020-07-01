2f32694d18a7651134ee5fc7d7281085f2dcbba9
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-07-01 22:39:19 +0200

    16-181 checking for duplicate email addresses

--- src/Udemy.Skinet.Api/Controllers/AccountController.cs
+++ src/Udemy.Skinet.Api/Controllers/AccountController.cs
@@ -49,6 +49,11 @@ namespace Udemy.Skinet.Api.Controllers {
 
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto) {
+
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value) {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email address is in use." } });
+            }
+
             var user = new AppUser {
                 DisplayName = registerDto.DisplayName,
                 Email = registerDto.Email,
