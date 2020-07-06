7e629082b401b7c6368ab8ee41298ebf8da501c6
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-07-06 23:26:25 +0200

    17-199 creating a register form

--- spa/client/src/app/account/register/register.component.ts
+++ spa/client/src/app/account/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
 
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      displayName: [null, [Validators.required]],
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(
+            '^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$'
+          ),
+        ],
+      ],
+      password: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/shop');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
