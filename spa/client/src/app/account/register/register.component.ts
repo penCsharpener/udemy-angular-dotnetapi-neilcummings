94b895c4cda85bd02cbe2acc7ee22b6a2db121a8
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-07-09 22:51:06 +0200

    17-201 async validation

--- spa/client/src/app/account/register/register.component.ts
+++ spa/client/src/app/account/register/register.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AsyncValidatorFn,
+} from '@angular/forms';
 import { AccountService } from '../account.service';
 import { Router } from '@angular/router';
+import { timer, of } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -33,6 +40,7 @@ export class RegisterComponent implements OnInit {
             '^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$'
           ),
         ],
+        [this.validateEmailNotTaken()],
       ],
       password: [null, Validators.required],
     });
@@ -48,5 +56,23 @@ export class RegisterComponent implements OnInit {
         this.errors = error.errors;
       }
     );
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control) => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if (!control.value) {
+            return of(null);
+          }
+
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map((res) => {
+              return res ? { emailExists: true } : null;
+            })
+          );
+        })
+      );
+    };
   }
 }
