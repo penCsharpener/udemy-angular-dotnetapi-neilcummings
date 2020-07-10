66eaf91c6a208f8e8051e2913a0d0e7dccf3aed2
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-07-10 21:35:31 +0200

    18-207 creating the order aggregate (models)

---
+++ src/Udemy.Skinet.Core/Entities/OrderAggregate/DeliveryMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace Udemy.Skinet.Core.Entities.OrderAggregate {
+    public class DeliveryMethod : BaseEntity {
+        public string ShortName { get; set; }
+        public string DeliveryTime { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/Udemy.Skinet.Core/Entities/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace Udemy.Skinet.Core.Entities.OrderAggregate {
+    public enum OrderStatus {
+        [EnumMember(Value = nameof(Pending))]
+        Pending,
+
+        [EnumMember(Value = "Payment Received")]
+        PaymentRecieved,
+
+        [EnumMember(Value = "Payment Failed")]
+        PaymentFailed
+    }
+}
